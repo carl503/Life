2b46bb747fd1de40f11f4e4b5f6e837fe8d46b6e
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:13:50 +0200

    first draft PlantEater implementation #4

---
+++ src/main/java/ch/zhaw/pm2/life/controller/PlantEater.java
@@ -0,0 +1,11 @@
+package ch.zhaw.pm2.life.controller;
+
+/**
+ * First Version of a PlantEater animal that extends AnimalObject
+ */
+public class PlantEater extends AnimalObject {
+
+    public PlantEater() {
+        super();
+    }
+}
