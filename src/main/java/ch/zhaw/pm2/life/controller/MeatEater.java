a163d604f79bd7c8aff4ef27a0c6986b5dbdeda1
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:13:34 +0200

    first draft MeatEater implementation #3

---
+++ src/main/java/ch/zhaw/pm2/life/controller/MeatEater.java
@@ -0,0 +1,13 @@
+package ch.zhaw.pm2.life.controller;
+
+/**
+ * First Version of a MeatEater Animal that extends AnimalObject
+ */
+public class MeatEater extends AnimalObject {
+
+    public MeatEater() {
+        super();
+    }
+
+
+}
