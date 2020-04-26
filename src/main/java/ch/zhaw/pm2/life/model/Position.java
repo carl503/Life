2de9553449755a8b1ead0ac3574852588beb62e1
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:03:43 +0200

    Position.java added to model #14

---
+++ src/main/java/ch/zhaw/pm2/life/model/Position.java
@@ -0,0 +1,15 @@
+package ch.zhaw.pm2.life.model;
+
+/**
+ * Position Objects represent the positions of the GameObjects in the field
+ */
+public class Position {
+    private int xValue;
+    private int yValue;
+
+
+    public Position(int xValue, int yValue){
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+}
