5104ad6f75ab0651427dc1db2e9daaf2cd838bf8
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:42:46 +0200

    FirstPlant first draft implemenation #16

---
+++ src/main/java/ch/zhaw/pm2/life/controller/FirstPlant.java
@@ -0,0 +1,13 @@
+package ch.zhaw.pm2.life.controller;
+
+/**
+ * Implementation of the first kind of plant
+ * Extends PlantObject
+ */
+public class FirstPlant extends PlantObject {
+
+    public FirstPlant() {
+        super();
+    }
+
+}
