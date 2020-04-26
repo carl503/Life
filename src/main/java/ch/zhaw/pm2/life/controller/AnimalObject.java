2844fab30ae39b667b2bf51022da01cbe02dc12f
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:05:09 +0200

    AnimalObject first implementation first draft #15

---
+++ src/main/java/ch/zhaw/pm2/life/controller/AnimalObject.java
@@ -0,0 +1,39 @@
+package ch.zhaw.pm2.life.controller;
+
+
+/**
+ * AnimalObjects extends GameObject
+ * AnimalObjects handles all features and methods that PlantEater and MeatEater have in common
+ */
+public abstract class AnimalObject extends GameObject {
+
+    public AnimalObject() {
+        super();
+    }
+
+    public void makeAMove(){
+        chooseRandomNeighbourField();
+        move();
+    }
+
+    /**
+     * Is called when the animal moves
+     */
+    public void move() {
+
+    }
+
+    /**
+     * Is called when the animal eats something
+     */
+    public void eat() {
+
+    }
+
+    /**
+     * Chooses 1 of the 8 or less fields around the AnimalObject
+     */
+    public void chooseRandomNeighbourField() {
+
+    }
+}
