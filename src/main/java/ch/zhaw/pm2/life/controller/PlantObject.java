18b9eeb7a2b0798bef2702cda673709ac0dd3473
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-04-26 22:06:14 +0200

    PlantObject first draft implementation #16

---
+++ src/main/java/ch/zhaw/pm2/life/controller/PlantObject.java
@@ -0,0 +1,19 @@
+package ch.zhaw.pm2.life.controller;
+
+/**
+ * PlantObject extends GameObject
+ * PlantObjects handles all features and methods that Future Plants will have in common
+ */
+public class PlantObject extends GameObject{
+
+    public PlantObject() {
+        super();
+    }
+
+    /**
+     * Makes a plant grow randomly on the field
+     */
+    public void growRandomlyOnField() {
+
+    }
+}
