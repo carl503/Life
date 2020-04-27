fc6b1c7a07dc1cde5097ec07a65eb83cbdf4b4db
meletlea <meletlea@students.zhaw.ch>
2020-04-27 14:54:10 +0200

    declare field dimension instead of calculate it multiple times

--- src/main/java/ch/zhaw/pm2/life/view/BoardView.java
+++ src/main/java/ch/zhaw/pm2/life/view/BoardView.java
@@ -3,6 +3,7 @@ package ch.zhaw.pm2.life.view;
 import ch.zhaw.pm2.life.controller.GameObject;
 import ch.zhaw.pm2.life.model.Board;
 import ch.zhaw.pm2.life.model.Position;
+import javafx.geometry.Dimension2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
@@ -14,6 +15,7 @@ public class BoardView extends Canvas {
     private final Board board;
     private final int width;
     private final int height;
+    private final Dimension2D fieldDimension;
 
     /**
      * Default constructor.
@@ -27,6 +29,7 @@ public class BoardView extends Canvas {
         this.board = board;
         setHeight(height);
         setWidth(width);
+        fieldDimension = new Dimension2D(width / (double) board.getColumns(), height / (double) board.getRows());
     }
 
     /**
@@ -44,13 +47,11 @@ public class BoardView extends Canvas {
 //
 //            gc.setFill(lifeForm.getColor());
             getGraphicsContext2D().setFill(Color.BLUE);
-            double maxWidth = width / (double) board.getColumns();
-            double maxHeight = height / (double) board.getRows();
             double scaling = 0.5;
             double translateFactor = (1 - scaling) * 0.5;
-            double translatedX = position.getX() + maxHeight * translateFactor;
-            double translatedY = position.getY() + maxWidth * translateFactor;
-            getGraphicsContext2D().fillOval(translatedX, translatedY, maxWidth * scaling, maxHeight * scaling);
+            double translatedX = position.getX() + fieldDimension.getHeight() * translateFactor;
+            double translatedY = position.getY() + fieldDimension.getWidth() * translateFactor;
+            getGraphicsContext2D().fillOval(translatedX, translatedY, fieldDimension.getWidth() * scaling, fieldDimension.getHeight() * scaling);
        }
     }
 
@@ -58,11 +59,11 @@ public class BoardView extends Canvas {
         getGraphicsContext2D().setLineWidth(1);
         getGraphicsContext2D().setFill(Color.BLACK);
 
-        for (int i = 0; i <= width; i+= width / board.getColumns()) {
+        for (int i = 0; i <= width; i+= fieldDimension.getWidth()) {
             getGraphicsContext2D().strokeLine(i, 0, i, height);
         }
 
-        for (int i = 0; i <= height; i+= height / board.getRows()) {
+        for (int i = 0; i <= height; i+= fieldDimension.getHeight()) {
             getGraphicsContext2D().strokeLine(0, i, width, i);
         }
     }
