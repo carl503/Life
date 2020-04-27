e4cae249094c7006df802f9dd0f0a240bc0292c3
meletlea <meletlea@students.zhaw.ch>
2020-04-27 16:59:44 +0200

    make constants public static final

--- src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
+++ src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
@@ -9,14 +9,14 @@ public class LifeWindowController {
 
     private final int width = 800;
     private final int height = 800;
-    private final int rows = 16;
-    private final int columns = 16;
+    public static final int ROWS = 16;
+    public static final int COLUMNS = 16;
 
     private BoardView boardView;
     @FXML VBox board;
 
     @FXML public void initialize() {
-        Board board = new Board(rows, columns);
+        Board board = new Board(ROWS, COLUMNS);
         boardView = new BoardView(width, height, board);
         this.board.getChildren().add(boardView);
         boardView.draw();
