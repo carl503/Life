203e713449f084a036e27461475dc060417caa3e
meletlea <meletlea@students.zhaw.ch>
2020-04-27 12:14:03 +0200

    Rename variable ui to boardView

--- src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
+++ src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
@@ -12,13 +12,13 @@ public class LifeWindowController {
     private final int rows = 16;
     private final int columns = 16;
 
-    private BoardView ui;
+    private BoardView boardView;
     @FXML VBox board;
 
     @FXML public void initialize() {
         Board board = new Board(rows, columns);
-        ui = new BoardView(width, height, board);
-        this.board.getChildren().add(ui);
-        ui.draw();
+        boardView = new BoardView(width, height, board);
+        this.board.getChildren().add(boardView);
+        boardView.draw();
     }
 }
