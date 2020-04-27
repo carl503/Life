c82e2fdfc9dc6113aa67284ca27a773a0d03305c
meletlea <meletlea@students.zhaw.ch>
2020-04-27 17:27:24 +0200

    override abstract methods

--- src/main/java/ch/zhaw/pm2/life/model/FirstPlant.java
+++ src/main/java/ch/zhaw/pm2/life/model/FirstPlant.java
@@ -10,4 +10,22 @@ public class FirstPlant extends PlantObject {
         super();
     }
 
+    /**
+     * Increases energy by a certain amount. Gets called when an animal eats a plant or another animal
+     * (Is called after a certain amount of turns - after X Energy the plants reproduces itself)
+     */
+    @Override
+    public void increaseEnergy() {
+
+    }
+
+    /**
+     * decreases energy by a certain amount. Gets called when an animal makes a move
+     * (or a plant is not fully eaten - phase - 2/3)
+     */
+    @Override
+    public void decreaseEnergy() {
+
+    }
+
 }
