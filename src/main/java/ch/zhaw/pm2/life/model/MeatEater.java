b728f2364280dd9623204edd25a61f3f425d7653
meletlea <meletlea@students.zhaw.ch>
2020-04-27 17:06:30 +0200

    override abstract methods

--- src/main/java/ch/zhaw/pm2/life/model/MeatEater.java
+++ src/main/java/ch/zhaw/pm2/life/model/MeatEater.java
@@ -9,5 +9,23 @@ public class MeatEater extends AnimalObject {
         super();
     }
 
+    /**
+     * Increases energy by a certain amount. Gets called when an animal eats a plant or another animal
+     * (Is called after a certain amount of turns - after X Energy the plants reproduces itself)
+     */
+    @Override
+    public void increaseEnergy() {
+
+    }
+
+    /**
+     * decreases energy by a certain amount. Gets called when an animal makes a move
+     * (or a plant is not fully eaten - phase - 2/3)
+     */
+    @Override
+    public void decreaseEnergy() {
+
+    }
+
 
 }
