b9b14acc8e40dd24f528a2cc54817f4508fa4e01
meletlea <meletlea@students.zhaw.ch>
2020-04-27 17:26:09 +0200

    make PlantObject an abstract class #16

--- src/main/java/ch/zhaw/pm2/life/model/PlantObject.java
+++ src/main/java/ch/zhaw/pm2/life/model/PlantObject.java
@@ -4,7 +4,7 @@ package ch.zhaw.pm2.life.model;
  * PlantObject extends GameObject
  * PlantObjects handles all features and methods that Future Plants will have in common
  */
-public class PlantObject extends GameObject{
+public abstract class PlantObject extends GameObject{
 
     protected static final int PLANT_ENERGY = 5;
 
@@ -12,28 +12,4 @@ public class PlantObject extends GameObject{
         super();
     }
 
-    /**
-     * Makes a plant grow randomly on the field
-     */
-    public void growRandomlyOnField() {
-
-    }
-
-    /**
-     * Increases energy by a certain amount. Gets called when an animal eats a plant or another animal
-     * (Is called after a certain amount of turns - after X Energy the plants reproduces itself)
-     */
-    @Override
-    public void increaseEnergy() {
-
-    }
-
-    /**
-     * decreases energy by a certain amount. Gets called when an animal makes a move
-     * (or a plant is not fully eaten - phase - 2/3)
-     */
-    @Override
-    public void decreaseEnergy() {
-
-    }
 }
