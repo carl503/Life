5a2ba18bfe69fb33217d4b38ae07d076ace0bd79
meletlea <meletlea@students.zhaw.ch>
2020-04-28 22:19:24 +0200

    apply changes from fxml in LifeWindowController

--- src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
+++ src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
@@ -2,8 +2,9 @@ package ch.zhaw.pm2.life.controller;
 
 import ch.zhaw.pm2.life.model.Board;
 import ch.zhaw.pm2.life.view.BoardView;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 
 public class LifeWindowController {
 
@@ -12,13 +13,18 @@ public class LifeWindowController {
     public static final int ROWS = 16;
     public static final int COLUMNS = 16;
 
+    @FXML private BorderPane rootPane;
+
     private BoardView boardView;
-    @FXML VBox board;
 
     @FXML public void initialize() {
         Board board = new Board(ROWS, COLUMNS);
         boardView = new BoardView(width, height, board);
-        this.board.getChildren().add(boardView);
         boardView.draw();
+        rootPane.setCenter(boardView);
+    }
+
+    @FXML private void close() {
+        Platform.exit();
     }
 }
