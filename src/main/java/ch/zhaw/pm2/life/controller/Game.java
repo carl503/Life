486194cdd67684f2598e7586865584fa31a41738
lubojcar <lubojcar@students.zhaw.ch>
2020-04-30 15:05:10 +0200

    added the stop method #22

--- src/main/java/ch/zhaw/pm2/life/controller/Game.java
+++ src/main/java/ch/zhaw/pm2/life/controller/Game.java
@@ -4,9 +4,14 @@ import ch.zhaw.pm2.life.model.board.Board;
 
 public class Game {
     private Board board;
+    private boolean ongoing = true;
 
     public Game(Board board) {
         this.board = board;
+    }
+
+    public void stop() {
+        ongoing = false;
     }
 
 }
