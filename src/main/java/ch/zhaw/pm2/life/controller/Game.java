3797fce93db4a2c55db4ee98f17e857e6f6ea87c
lubojcar <lubojcar@students.zhaw.ch>
2020-04-30 21:12:29 +0200

    added lifeformCount to the constructor #30

--- src/main/java/ch/zhaw/pm2/life/controller/Game.java
+++ src/main/java/ch/zhaw/pm2/life/controller/Game.java
@@ -3,11 +3,17 @@ package ch.zhaw.pm2.life.controller;
 import ch.zhaw.pm2.life.model.Board;
 
 public class Game {
-    private Board board;
     private boolean ongoing = true;
+    private Board board;
+    private int plantCount;
+    private int meatEaterCount;
+    private int plantEaterCount;
 
-    public Game(Board board) {
+    public Game(Board board, int plantCount, int meatEaterCount, int plantEaterCount) {
         this.board = board;
+        this.plantCount = plantCount;
+        this.meatEaterCount = meatEaterCount;
+        this.plantEaterCount = plantEaterCount;
     }
 
     public void init() {
