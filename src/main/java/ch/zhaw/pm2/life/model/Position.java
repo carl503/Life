9a2933d0bef95aa4a1465a4198ba1b90954ede66
meletlea <meletlea@students.zhaw.ch>
2020-05-01 14:39:22 +0200

    override equals and hash for position class

--- src/main/java/ch/zhaw/pm2/life/model/Position.java
+++ src/main/java/ch/zhaw/pm2/life/model/Position.java
@@ -1,5 +1,7 @@
 package ch.zhaw.pm2.life.model;
 
+import java.util.Objects;
+
 /**
  * Position Objects represent the positions of the GameObjects in the field
  */
@@ -20,4 +22,19 @@ public class Position {
     public int getY() {
         return yValue;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return xValue == position.xValue &&
+                yValue == position.yValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xValue, yValue);
+    }
+
 }
