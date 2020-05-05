9f9966b18e34c102cb6932cdc6e31ebaf6be0d23
lubojcar <lubojcar@students.zhaw.ch>
2020-05-05 13:51:59 +0200

    implemented eat method for meat eater and plant eater

--- src/main/java/ch/zhaw/pm2/life/model/lifeform/animal/MeatEater.java
+++ src/main/java/ch/zhaw/pm2/life/model/lifeform/animal/MeatEater.java
@@ -1,6 +1,10 @@
 package ch.zhaw.pm2.life.model.lifeform.animal;
 
+import ch.zhaw.pm2.life.exception.LifeFormException;
+import ch.zhaw.pm2.life.model.lifeform.LifeForm;
 import javafx.scene.paint.Color;
+
+import java.util.Objects;
 
 /**
  * First Version of a MeatEater Animal that extends AnimalObject
@@ -16,4 +20,16 @@ public class MeatEater extends AnimalObject {
         isMale = setGender();
     }
 
+    @Override
+    public void eat(LifeForm lifeForm) throws LifeFormException {
+        Objects.requireNonNull(lifeForm, "Cannot eat null.");
+
+        if(lifeForm.getFoodType() == FoodType.PLANT) {
+            throw new LifeFormException("Cannot eat this plant. Do I look like a vegetarian?!");
+        } else if (lifeForm instanceof MeatEater && lifeForm.getCurrentEnergy() > this.getCurrentEnergy()) {
+            throw new LifeFormException("Cannot eat this meat eater. He is stronger than I.");
+        }
+        increaseEnergy(lifeForm.getCurrentEnergy());
+        lifeForm.die();
+    }
 }
--- src/main/java/ch/zhaw/pm2/life/model/lifeform/animal/PlantEater.java
+++ src/main/java/ch/zhaw/pm2/life/model/lifeform/animal/PlantEater.java
@@ -1,6 +1,10 @@
 package ch.zhaw.pm2.life.model.lifeform.animal;
 
+import ch.zhaw.pm2.life.exception.LifeFormException;
+import ch.zhaw.pm2.life.model.lifeform.LifeForm;
 import javafx.scene.paint.Color;
+
+import java.util.Objects;
 
 /**
  * First Version of a PlantEater animal that extends AnimalObject
@@ -16,4 +20,13 @@ public class PlantEater extends AnimalObject {
         isMale = setGender();
     }
 
+    @Override
+    public void eat(LifeForm lifeForm) throws LifeFormException {
+        Objects.requireNonNull(lifeForm, "Cannot eat null.");
+        if(lifeForm.getFoodType() == FoodType.MEAT) {
+            throw new LifeFormException("Cannot eat this meat, I am vegetarian.");
+        }
+        increaseEnergy(lifeForm.getCurrentEnergy());
+        lifeForm.die();
+    }
 }
