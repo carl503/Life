d39416c36b7e35cc74061d857f08af0c4d5ab879
lubojcar <lubojcar@students.zhaw.ch>
2020-05-14 15:57:45 +0200

    removed fixed size and added eventlistener + mehtod updateSize #40

--- src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
+++ src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
@@ -5,7 +5,7 @@ import ch.zhaw.pm2.life.view.BoardView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 
 import java.util.Objects;
 
@@ -16,15 +16,13 @@ public class LifeWindowController {
 
     private static final int ROWS = 16;
     private static final int COLUMNS = 16;
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 800;
 
     private BoardView boardView;
     private Game game;
     private SetupController setupController;
     private Board boardObject;
 
-    @FXML private VBox board;
+    @FXML private Pane board;
     @FXML private TextArea messageField;
     @FXML private Button nextRoundButton;
     @FXML private Button stopSimButton;
@@ -36,8 +34,10 @@ public class LifeWindowController {
     public void initialize() {
         try {
             boardObject = new Board(ROWS, COLUMNS);
-            boardView = new BoardView(WIDTH, HEIGHT, boardObject);
-            this.board.getChildren().add(0, boardView);
+            boardView = new BoardView(boardObject);
+            board.getChildren().add(boardView);
+            board.widthProperty().addListener(observable -> updateSize());
+            board.heightProperty().addListener(observable -> updateSize());
         } catch (Exception e) {
             messageField.appendText(e.getMessage() + "\n");
         }
@@ -61,6 +61,15 @@ public class LifeWindowController {
             game.stop();
             nextRoundButton.setDisable(true);
             stopSimButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void updateSize() {
+        double newWidth = board.getWidth();
+        double newHeight = board.getHeight();
+        if (newHeight > 0.0 && newWidth > 0.0) {
+            boardView.updateDimension(newWidth, newHeight);
         }
     }
 
