844a5a13fdd6bbb41bbc235107325c74071078aa
randjpat <randjpat@students.zhaw.ch>
2020-05-14 23:14:42 +0200

    #39 GameObjectTest updated

--- src/test/java/ch/zhaw/pm2/life/model/GameObjectTest.java
+++ src/test/java/ch/zhaw/pm2/life/model/GameObjectTest.java
@@ -2,7 +2,9 @@ package ch.zhaw.pm2.life.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,13 +16,12 @@ import static org.mockito.Mockito.*;
 public class GameObjectTest {
     private static final int BOARD_SIZE = 3;
 
-    private GameObject gameObject;
-    private Random random;
+    @Spy private GameObject gameObject;
+    @Mock private Random random;
 
     @BeforeEach
     public void setUp() {
-        random = mock(Random.class);
-        gameObject = Mockito.mock(GameObject.class, Mockito.CALLS_REAL_METHODS);
+        MockitoAnnotations.initMocks(this);
 
         gameObject.setRandom(random);
         gameObject.setColumns(BOARD_SIZE);
@@ -43,7 +44,7 @@ public class GameObjectTest {
         }
 
         for (int i = 0; i < expectedNeighbours.size(); i++) {
-            when(random.nextInt(expectedNeighbours.size())).thenReturn(i);
+            when(random.nextInt(anyInt())).thenReturn(i);
             Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
             assertTrue(expectedNeighbours.contains(neighbour));
         }
@@ -61,7 +62,25 @@ public class GameObjectTest {
         }
 
         for (int i = 0; i < expectedNeighbours.size(); i++) {
-            when(random.nextInt(expectedNeighbours.size())).thenReturn(i);
+            when(random.nextInt(anyInt())).thenReturn(i);
+            Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
+            assertTrue(expectedNeighbours.contains(neighbour));
+        }
+    }
+
+    @Test
+    public void testChooseRandomNeighbourPositionCenterTopStart() {
+        gameObject.setPosition(new Vector2D(1, 0));
+
+        List<Vector2D> expectedNeighbours = new ArrayList<>();
+        for (int y = 0; y < BOARD_SIZE - 1; y++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                expectedNeighbours.add(new Vector2D(x, y));
+            }
+        }
+
+        for (int i = 0; i < expectedNeighbours.size(); i++) {
+            when(random.nextInt(anyInt())).thenReturn(i);
             Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
             assertTrue(expectedNeighbours.contains(neighbour));
         }
@@ -79,14 +98,32 @@ public class GameObjectTest {
         }
 
         for (int i = 0; i < expectedNeighbours.size(); i++) {
-            when(random.nextInt(expectedNeighbours.size())).thenReturn(i);
+            when(random.nextInt(anyInt())).thenReturn(i);
             Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
             assertTrue(expectedNeighbours.contains(neighbour));
         }
     }
 
     @Test
-    public void testChooseRandomNeighbourPositionUnderLeftCornerStart() {
+    public void testChooseRandomNeighbourPositionCenterRightStart() {
+        gameObject.setPosition(new Vector2D(2, 1));
+
+        List<Vector2D> expectedNeighbours = new ArrayList<>();
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            for (int x = 1; x < BOARD_SIZE; x++) {
+                expectedNeighbours.add(new Vector2D(x, y));
+            }
+        }
+
+        for (int i = 0; i < expectedNeighbours.size(); i++) {
+            when(random.nextInt(anyInt())).thenReturn(i);
+            Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
+            assertTrue(expectedNeighbours.contains(neighbour));
+        }
+    }
+
+    @Test
+    public void testChooseRandomNeighbourPositionBottomLeftCornerStart() {
         gameObject.setPosition(new Vector2D(0, 2));
 
         List<Vector2D> expectedNeighbours = new ArrayList<>();
@@ -97,14 +134,32 @@ public class GameObjectTest {
         }
 
         for (int i = 0; i < expectedNeighbours.size(); i++) {
-            when(random.nextInt(expectedNeighbours.size())).thenReturn(i);
+            when(random.nextInt(anyInt())).thenReturn(i);
             Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
             assertTrue(expectedNeighbours.contains(neighbour));
         }
     }
 
     @Test
-    public void testChooseRandomNeighbourPositionUnderRightCornerStart() {
+    public void testChooseRandomNeighbourPositionCenterBottomStart() {
+        gameObject.setPosition(new Vector2D(1, 2));
+
+        List<Vector2D> expectedNeighbours = new ArrayList<>();
+        for (int y = 1; y < BOARD_SIZE; y++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                expectedNeighbours.add(new Vector2D(x, y));
+            }
+        }
+
+        for (int i = 0; i < expectedNeighbours.size(); i++) {
+            when(random.nextInt(anyInt())).thenReturn(i);
+            Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
+            assertTrue(expectedNeighbours.contains(neighbour));
+        }
+    }
+
+    @Test
+    public void testChooseRandomNeighbourPositionBottomRightCornerStart() {
         gameObject.setPosition(new Vector2D(2, 2));
 
         List<Vector2D> expectedNeighbours = new ArrayList<>();
@@ -115,10 +170,56 @@ public class GameObjectTest {
         }
 
         for (int i = 0; i < expectedNeighbours.size(); i++) {
-            when(random.nextInt(expectedNeighbours.size())).thenReturn(i);
+            when(random.nextInt(anyInt())).thenReturn(i);
             Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
             assertTrue(expectedNeighbours.contains(neighbour));
         }
+    }
+
+    @Test
+    public void testChooseRandomNeighbourPositionCenterLeftStart() {
+        gameObject.setPosition(new Vector2D(0, 1));
+
+        List<Vector2D> expectedNeighbours = new ArrayList<>();
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            for (int x = 0; x < BOARD_SIZE - 1; x++) {
+                expectedNeighbours.add(new Vector2D(x, y));
+            }
+        }
+
+        for (int i = 0; i < expectedNeighbours.size(); i++) {
+            when(random.nextInt(anyInt())).thenReturn(i);
+            Vector2D neighbour = gameObject.chooseRandomNeighbourPosition();
+            assertTrue(expectedNeighbours.contains(neighbour));
+        }
+    }
+
+    @Test
+    public void increaseEnergyPositiveInput() {
+        int energy = gameObject.energy;
+        gameObject.increaseEnergy(10);
+        assertEquals(energy + 10, gameObject.energy);
+    }
+
+    @Test
+    public void increaseEnergyZeroInput() {
+        int energy = gameObject.energy;
+        gameObject.increaseEnergy(0);
+        assertEquals(energy, gameObject.energy);
+    }
+
+    @Test
+    public void decreaseEnergyPositiveInput() {
+        int energy = gameObject.energy;
+        gameObject.decreaseEnergy(10);
+        assertEquals(energy - 10, gameObject.energy);
+    }
+
+    @Test
+    public void decreaseEnergyZeroInput() {
+        int energy = gameObject.energy;
+        gameObject.decreaseEnergy(0);
+        assertEquals(energy, gameObject.energy);
     }
 
     //==================================================================================================================
@@ -131,5 +232,17 @@ public class GameObjectTest {
         when(random.nextInt(2)).then(CALLS_REAL_METHODS);
 
         assertThrows(IndexOutOfBoundsException.class, () -> gameObject.chooseRandomNeighbourPosition());
+    }
+
+    @Test
+    public void increaseEnergyNegativeInputInvalid() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> gameObject.increaseEnergy(-10));
+        assertEquals("Energy cannot increase with a negative value.", exception.getMessage());
+    }
+
+    @Test
+    public void decreaseEnergyNegativeInputInvalid() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> gameObject.decreaseEnergy(-10));
+        assertEquals("Energy cannot decrease with a negative value.", exception.getMessage());
     }
 }
