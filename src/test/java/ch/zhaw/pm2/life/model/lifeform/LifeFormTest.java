4cd582267adae9631b8ffad3920b05eb7b5509a1
Nino Pedergnana <pedergnana.nino@gmail.com>
2020-05-14 22:34:43 +0200

    LifeFormTest refactoring. Auslagerung von lifeform und keine Spezifischen lifeforms werden mehr verwendet #38

--- src/test/java/ch/zhaw/pm2/life/model/lifeform/LifeFormTest.java
+++ src/test/java/ch/zhaw/pm2/life/model/lifeform/LifeFormTest.java
@@ -1,8 +1,10 @@
 package ch.zhaw.pm2.life.model.lifeform;
 
-import ch.zhaw.pm2.life.model.lifeform.animal.Carnivore;
-import ch.zhaw.pm2.life.model.lifeform.plant.Plant;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -11,6 +13,13 @@ import static org.mockito.Mockito.*;
 
 
 public class LifeFormTest {
+    
+    @Spy private LifeForm lifeform;
+    
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     //==================================================================================================================
     // Positive tests
@@ -18,62 +27,55 @@ public class LifeFormTest {
 
     @Test
     public void testLifeFormDie() {
-        Plant plant = spy(Plant.class);
-        plant.die();
-        assertTrue(plant.isDead());
+        lifeform.die();
+        assertTrue(lifeform.isDead());
     }
 
     @Test
     public void testLifeFormBecomePoisoned() {
-        Plant plant = spy(Plant.class);
-        plant.becomePoisoned();
-        assertTrue(plant.isPoisoned());
+        lifeform.becomePoisoned();
+        assertTrue(lifeform.isPoisoned());
     }
 
     @Test
     public void testLifeFormGetPoisonedEnergyConsumption() {
-        Plant plant = spy(Plant.class);
-        plant.setEnergy(30);
-        plant.becomePoisoned();
-        assertEquals(9, plant.getPoisonedEnergyConsumption());
+        lifeform.setEnergy(30);
+        lifeform.becomePoisoned();
+        assertEquals(9, lifeform.getPoisonedEnergyConsumption());
     }
 
     @Test
     public void testLifeFormNotPoisonedAnymore() {
-        Plant plant = spy(Plant.class);
-        plant.setEnergy(30);
-        plant.becomePoisoned();
-        assertTrue(plant.isPoisoned());
+        lifeform.setEnergy(30);
+        lifeform.becomePoisoned();
+        assertTrue(lifeform.isPoisoned());
         // remove poison
         for(int i = 0; i < 10; i++) {
-            plant.getPoisonedEnergyConsumption();
+            lifeform.getPoisonedEnergyConsumption();
         }
-        assertFalse(plant.isPoisoned());
+        assertFalse(lifeform.isPoisoned());
     }
 
     @Test
     public void testLifeFormIsPoisonous() {
-        Plant plant = spy(Plant.class);
-        plant.isPoisonous = true;
-        assertTrue(plant.isPoisonous());
+        lifeform.isPoisonous = true;
+        assertTrue(lifeform.isPoisonous());
     }
 
     @Test
     public void testLifeFormGenderPlant() {
-        Plant plant = spy(Plant.class);
-        assertEquals("N", plant.getGender());
+        assertEquals("N", lifeform.getGender());
     }
 
     @Test
     public void testLifeFormGenderCarnivore() {
-        Carnivore carnivore = spy(Carnivore.class);
-        assertThat(carnivore.getGender(), anyOf(is("F"), is("M")));
+        assertThat(lifeform.getGender(), anyOf(is("F"), is("M")));
     }
 
     @Test
     public void testLifeFormGetFoodType() {
-        LifeForm lifeForm = spy(Carnivore.class);
-        assertThat(lifeForm.getFoodType(), anyOf(is(LifeForm.FoodType.MEAT), is(LifeForm.FoodType.PLANT)));
+        when(lifeform.getFoodType()).thenReturn(LifeForm.FoodType.PLANT);
+        assertEquals(LifeForm.FoodType.PLANT, lifeform.getFoodType());
     }
 
 }
