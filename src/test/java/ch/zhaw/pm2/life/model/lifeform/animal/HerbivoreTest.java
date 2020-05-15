8b3bead08e78b2343f1085a04beda1dd590654a5
randjpat <randjpat@students.zhaw.ch>
2020-05-15 16:53:05 +0200

    #99 HerbivoreTest added

---
+++ src/test/java/ch/zhaw/pm2/life/model/lifeform/animal/HerbivoreTest.java
@@ -0,0 +1,149 @@
+package ch.zhaw.pm2.life.model.lifeform.animal;
+
+import ch.zhaw.pm2.life.exception.LifeFormException;
+import ch.zhaw.pm2.life.model.GameObject;
+import ch.zhaw.pm2.life.model.Vector2D;
+import ch.zhaw.pm2.life.model.lifeform.LifeForm;
+import ch.zhaw.pm2.life.model.lifeform.LifeFormActionCheck;
+import ch.zhaw.pm2.life.model.lifeform.plant.Plant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class HerbivoreTest {
+    private Herbivore herbivore;
+    private Set<GameObject> dummyNeighbourSet;
+    private Plant plant;
+
+    @Spy private GameObject gameObject;
+    @Spy private LifeForm lifeForm;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        herbivore = new Herbivore();
+        dummyNeighbourSet = new HashSet<>();
+        plant = new Plant();
+
+        herbivore.setPosition(new Vector2D(1, 1));
+    }
+
+
+    //==================================================================================================================
+    // Positive tests
+    //==================================================================================================================
+
+    @Test
+    public void getNearestNeighbourEmptyGameObjectSet() {
+        assertNull(herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getNearestNeighbourOnePlant() {
+        plant.setPosition(new Vector2D(0, 1));
+        dummyNeighbourSet.add(plant);
+
+        assertEquals(plant.getPosition(), herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getNearestNeighbourTwoPlants() {
+        Plant plant2 = new Plant();
+
+        plant.setPosition(new Vector2D(0, 1));
+        plant2.setPosition(new Vector2D(2, 0));
+        dummyNeighbourSet.add(plant);
+        dummyNeighbourSet.add(plant2);
+
+        assertEquals(plant.getPosition(), herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getNearestNeighbourOnePlantOneGameObject() {
+        plant.setPosition(new Vector2D(0, 0));
+        gameObject.setPosition(new Vector2D(0, 1));
+        dummyNeighbourSet.add(plant);
+        dummyNeighbourSet.add(gameObject);
+
+        assertEquals(plant.getPosition(), herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getNearestNeighbourNoNearestPlants() {
+        gameObject.setPosition(new Vector2D(0, 1));
+        dummyNeighbourSet.add(gameObject);
+
+        assertNull(herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getEatRulesEatPlantObject() {
+        LifeFormActionCheck lifeFormActionCheck = herbivore.getEatRules(plant);
+        assertDoesNotThrow(lifeFormActionCheck::check);
+    }
+
+    @Test
+    public void getEatRulesEatPlant() {
+        when(lifeForm.getFoodType()).thenReturn(LifeForm.FoodType.PLANT);
+        LifeFormActionCheck lifeFormActionCheck = herbivore.getEatRules(lifeForm);
+
+        assertDoesNotThrow(lifeFormActionCheck::check);
+    }
+
+    @Test
+    public void getEatRulesEatHerbivore() {
+        LifeFormActionCheck lifeFormActionCheck = herbivore.getEatRules(herbivore);
+
+        Exception exception = assertThrows(LifeFormException.class, lifeFormActionCheck::check);
+        assertEquals("Cannot eat this meat, I am vegetarian.", exception.getMessage());
+    }
+
+    @Test
+    public void getEatRulesEatMeat() {
+        when(lifeForm.getFoodType()).thenReturn(LifeForm.FoodType.MEAT);
+        LifeFormActionCheck lifeFormActionCheck = herbivore.getEatRules(lifeForm);
+
+        Exception exception = assertThrows(LifeFormException.class, lifeFormActionCheck::check);
+        assertEquals("Cannot eat this meat, I am vegetarian.", exception.getMessage());
+    }
+
+
+    //==================================================================================================================
+    // Negative tests
+    //==================================================================================================================
+
+    @Test
+    public void getNearestNeighbourNullGameObjectSet() {
+        assertThrows(NullPointerException.class, () -> herbivore.getNearestNeighbour(null));
+    }
+
+    @Test
+    public void getNearestNeighbourPlantSameHerbivorePosition() {
+        plant.setPosition(herbivore.getPosition());
+        dummyNeighbourSet.add(plant);
+
+        assertEquals(herbivore.getPosition(), herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getNearestNeighbourGameObjectSameHerbivorePosition() {
+        gameObject.setPosition(herbivore.getPosition());
+        dummyNeighbourSet.add(gameObject);
+
+        assertNull(herbivore.getNearestNeighbour(dummyNeighbourSet));
+    }
+
+    @Test
+    public void getEatRulesEatPlantEatLifeForm() {
+        LifeFormActionCheck lifeFormActionCheck = herbivore.getEatRules(lifeForm);
+
+        assertDoesNotThrow(lifeFormActionCheck::check);
+    }
+}
