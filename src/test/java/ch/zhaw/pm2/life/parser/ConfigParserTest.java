f6f9633c6b48746b5094fc617240c6bf79513c7e
meletlea <meletlea@students.zhaw.ch>
2020-05-15 15:19:42 +0200

    #101 use constants

--- src/test/java/ch/zhaw/pm2/life/parser/ConfigParserTest.java
+++ src/test/java/ch/zhaw/pm2/life/parser/ConfigParserTest.java
@@ -16,6 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ConfigParserTest {
 
+    private static final String COULD_NOT_PARSE_THE_CONFIG_FILE = "Could not parse the config file";
+    private static final String CARNIVORE = "carnivore";
+    private static final String WOLF = "Wolf";
+    private static final String WOLF_COLOR = "#D3D3D3";
+    private static final String WOLF_TYPE = "wolf.type";
+    private static final String WOLF_NAME = "wolf.name";
+    private static final String WOLF_ENERGY_KEY = "wolf.energy";
+    private static final String WOLF_COLOR_KEY = "wolf.color";
+    private static final String HASE = "Hase";
+    private static final String DANDELION = "Löwenzahn";
+    private static final String RABBIT_COLOR = "#8AF1FE";
+    private static final String DANDELION_COLOR = "#FAFE4B";
+    private static final int WOLF_ENERGY_VALUE = 7;
+    private static final int DANDELION_ENERGY = 20;
+    private static final int RABBIT_ENERGY = 4;
+
     @Test
     public void testInstanceNotNull() {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
@@ -31,7 +47,7 @@ public class ConfigParserTest {
         List<GameObject> actual = assertDoesNotThrow(parser::parseObjects);
 
         assertEquals(expected.size(), actual.size());
-        for(int i = 0; i < expected.size(); i++) {
+        for (int i = 0; i < expected.size(); i++) {
             LifeForm expectedLifeForm = (LifeForm) expected.get(i);
             LifeForm actualLifeForm = (LifeForm) actual.get(i);
             assertEquals(expectedLifeForm.getName(), actualLifeForm.getName());
@@ -45,7 +61,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidType());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -60,7 +76,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidTextValue());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -89,7 +105,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidWrongColorHexFive());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -97,7 +113,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidWrongColorHexFour());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -105,7 +121,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidWrongColorHexTwo());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -113,7 +129,7 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidWrongColorHexSeven());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     @Test
@@ -121,122 +137,122 @@ public class ConfigParserTest {
         ConfigParser parser = assertDoesNotThrow(ConfigParser::getInstance);
         parser.setConfigProperties(loadInvalidWrongColorHexUnsupportedChar());
         Exception thrown = assertThrows(LifeException.class, parser::parseObjects);
-        assertEquals("Could not parse the config file", thrown.getMessage());
+        assertEquals(COULD_NOT_PARSE_THE_CONFIG_FILE, thrown.getMessage());
     }
 
     private Properties loadInvalidWrongColorHexUnsupportedChar() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#00X");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, "#00X");
         return config;
     }
 
     private Properties loadInvalidWrongColorHexSeven() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3Abdg4");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, "#D3Abdg4");
         return config;
     }
 
     private Properties loadInvalidWrongColorHexTwo() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, "#D3");
         return config;
     }
 
     private Properties loadInvalidWrongColorHexFour() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, "#D3D3");
         return config;
     }
 
     private Properties loadInvalidWrongColorHexFive() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, "#D3D3D");
         return config;
     }
 
     private Properties loadInvalidMissingPropertyString() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadInvalidMissingPropertyNumber() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadInvalidNumberValue() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", "a");
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, "a");
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadInvalidTextValue() {
         Properties config = new Properties();
-        config.put("wolf.type", "\"carnivore\"");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, "\"carnivore\"");
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadInvalidConfigKey() {
         Properties config = new Properties();
-        config.put("wolf.hello.world", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D3");
+        config.put("wolf.hello.world", CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadInvalidType() {
         Properties config = new Properties();
-        config.put("wolf.type", "test");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, "test");
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
         return config;
     }
 
     private Properties loadValidConfig() {
         Properties config = new Properties();
-        config.put("wolf.type", "carnivore");
-        config.put("wolf.name", "Wolf");
-        config.put("wolf.energy", 7);
-        config.put("wolf.color", "#D3D3D3");
+        config.put(WOLF_TYPE, CARNIVORE);
+        config.put(WOLF_NAME, WOLF);
+        config.put(WOLF_ENERGY_KEY, WOLF_ENERGY_VALUE);
+        config.put(WOLF_COLOR_KEY, WOLF_COLOR);
 
         config.put("rabbit.type", "herbivore");
-        config.put("rabbit.name", "Hase");
-        config.put("rabbit.energy", 4);
-        config.put("rabbit.color", "#8AF1FE");
+        config.put("rabbit.name", HASE);
+        config.put("rabbit.energy", RABBIT_ENERGY);
+        config.put("rabbit.color", RABBIT_COLOR);
 
         config.put("dandelion.type", "plant");
-        config.put("dandelion.name", "Löwenzahn");
-        config.put("dandelion.color", "#FAFE4B");
-        config.put("dandelion.energy", 20);
+        config.put("dandelion.name", DANDELION);
+        config.put("dandelion.color", DANDELION_COLOR);
+        config.put("dandelion.energy", DANDELION_ENERGY);
 
         return config;
     }
@@ -245,21 +261,21 @@ public class ConfigParserTest {
         List<GameObject> expected = new ArrayList<>();
 
         LifeForm bear = new Carnivore();
-        bear.setName("Wolf");
-        bear.setEnergy(7);
-        bear.setColor("#D3D3D3");
+        bear.setName(WOLF);
+        bear.setEnergy(WOLF_ENERGY_VALUE);
+        bear.setColor(WOLF_COLOR);
         expected.add(bear);
 
         LifeForm rabbit = new Herbivore();
-        rabbit.setName("Hase");
-        rabbit.setEnergy(4);
-        rabbit.setColor("#8AF1FE");
+        rabbit.setName(HASE);
+        rabbit.setEnergy(RABBIT_ENERGY);
+        rabbit.setColor(RABBIT_COLOR);
         expected.add(rabbit);
 
         LifeForm dandelion = new Plant();
-        dandelion.setName("Löwenzahn");
-        dandelion.setEnergy(20);
-        dandelion.setColor("#FAFE4B");
+        dandelion.setName(DANDELION);
+        dandelion.setEnergy(DANDELION_ENERGY);
+        dandelion.setColor(DANDELION_COLOR);
         expected.add(dandelion);
 
         return expected;
