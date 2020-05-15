2fa2dc9606adf20d6e082fe7f974d646d83f78df
lubojcar <lubojcar@students.zhaw.ch>
2020-05-15 20:06:07 +0200

    Removed unused import

--- src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
+++ src/main/java/ch/zhaw/pm2/life/controller/LifeWindowController.java
@@ -11,7 +11,6 @@ import ch.zhaw.pm2.life.view.StatisticView;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
